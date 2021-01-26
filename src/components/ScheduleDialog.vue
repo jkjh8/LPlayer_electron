ed7693cf5236f391598329cf038aa8da1670ed1d
jkjh8 <jkjh82@naver.com>
2021-01-26 15:26:58 +0900

    1

--- src/components/ScheduleDialog.vue
+++ src/components/ScheduleDialog.vue
@@ -1,7 +1,24 @@
 <template>
-  <q-card style="width: 400px">
+  <q-card style="width: 50%">
     <q-card-section>
       <div class="text-h6">Add Schedule</div>
     </q-card-section>
+    <q-card-section>
+      <div class="q-mx-md" style="max-width: 90%">
+        <q-input v-model="name" label="Schedule Name" />
+        <q-file v-model="file" label="Select File" />
+      </div>
+    </q-card-section>
   </q-card>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      file: ''
+    }
+  }
+}
+</script>
