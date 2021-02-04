3c9ecc6bde68128f1e925d395abef1f0e49cd22d
jkjh8 <jkjh82@naver.com>
2021-02-04 17:57:58 +0900

    schedule name

--- src/mixins/log.js
+++ src/mixins/log.js
@@ -14,7 +14,7 @@ export const log = {
       const msg = {
         source: `Player ${this.status.booth - 9}`,
         category: 'Schedule',
-        message: `Schedule ${action}, Mode: ${message.mode}, Weeks: ${message.weeks.map(e => e.label).join(',')}, Time: ${message.time}, file: ${message.file.path}, Zone: ${message.zones.map(e => e.name).join(',')}, Eanble: ${message.enable}`
+        message: `Schedule ${action}, Name: ${message.name}, Mode: ${message.mode}, Weeks: ${message.weeks.map(e => e.label).join(',')}, Time: ${message.time}, file: ${message.file.path}, Zone: ${message.zones.map(e => e.name).join(',')}, Eanble: ${message.enable}`
       }
       ipcRenderer.send('log', msg)
     }
